b5389d4091efc6bcd22da298b3d8683ed69a4e3d
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-04-08 16:28:06 -0700

    All tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *My hello, printf task
+  *Return: 1 after program execution
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
