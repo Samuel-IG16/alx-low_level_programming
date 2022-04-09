53a43323834c46a3727cac1a47e5c4a72fb40ccc
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-04-08 17:25:31 -0700

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,16 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
-	if (n==0)
+	if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-	else if (n<0)
+	else if (n < 0)
 	{
 		printf("%i is negative\n", n);
 	}
