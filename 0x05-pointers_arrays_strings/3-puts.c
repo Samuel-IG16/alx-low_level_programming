1a5cec3e5381444d8e6846c0a535e0f0dbc6919f
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-04-20 13:58:24 -0700

    Fix error on Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse,
- * followed by a new line.
- * @s: an input string
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
  * Return: Nothing
  */
-void print_rev(char *s)
+void _puts(char *str)
 {
-	int len = 0;
+	int i = 0;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (len)
-		_putchar(s[--len]);
-
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
