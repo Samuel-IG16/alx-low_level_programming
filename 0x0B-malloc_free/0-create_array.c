ea373febd912b54704476178f3f46f8cfcd8ab52
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-05-06 17:10:01 -0700

    new

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,16 +11,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	unsigned int i = 0;
-	
+	unsigned int i = 0;	
 	s = malloc(size * sizeof(char));
-	
 	if (s == NULL)
 		return (NULL);
-	
 	if (size == 0)
 		return (NULL);
-	
 	while (i < size)
 	{
 		s[i] = c;
