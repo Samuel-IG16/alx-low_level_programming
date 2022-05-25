ecd2f87d4997e9154e7243c4462c3294cd77e3dd
Igbinijesu Samuel <65024899+Samuel-IG16@users.noreply.github.com>
2022-05-25 20:03:33 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,29 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success/correct)
- */
+  * Alx - SE Tasks
+  * Author: Igbinijesu Samuel
+  *
+  * main - entry point of code 
+  * return 1 at the end of main in this case
+  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-	else
-	{
-		printf("%i is positive\n", n);
-	}
-	return (0);
+	if (n > 0)
+        	printf("%i is positive\n", n);
+    	else if (n < 0)
+        	printf("%i is negative\n", n);
+    	else
+        	printf("%i is zero\n", n);
+	return 0;
 }
