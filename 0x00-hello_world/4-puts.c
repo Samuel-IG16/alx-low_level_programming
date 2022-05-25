3ceb7a31be9216c36ed81c8dc81ee3fcb37a999c
Igbinijesu Samuel <65024899+Samuel-IG16@users.noreply.github.com>
2022-05-25 17:41:44 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
-  *main -entry point
+  * Alx - SE Tasks
+  * Author: Igbinijesu Samuel
   *
-  *Return: 0 after printing the function
+  * main - entry point of code 
+  * return 0 at the end of main
   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return 0;
 }
 
