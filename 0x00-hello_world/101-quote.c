013a5529b869e3d0664192b58c45b5d5c2f24adf
Igbinijesu Samuel <65024899+Samuel-IG16@users.noreply.github.com>
2022-05-25 19:37:42 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
   * Author: Igbinijesu Samuel
   *
   * main - entry point of code 
-  * return 0 always at the end of main
- */
+  * return 1 at the end of main in this case
+  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return 1;
 }
