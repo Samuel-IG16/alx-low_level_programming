7e789bdf49d442137ef747c7d8300825949beff6
Igbinijesu Samuel <65024899+Samuel-IG16@users.noreply.github.com>
2022-05-30 15:31:33 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,28 @@
 #include "main.h"
-
 /**
- * print_sign - Check main
- * @n: An input number
- * Description: This function prints the sign of the number
- * Return: 1 if number is positive, o if number is 0, or
- * -1 if number is negative
- */
+  * Alx - SE Tasks
+  * Author: Igbinijesu Samuel
+  *
+  * return 1 if number is positive or 0 if number is zero or -1 if number is negative
+  */
 int print_sign(int n)
 {
-	int value;
+	int signvalue;
 
-	if (n > 0)
-	{
-		value = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		value = 0;
-		_putchar('0');
-	}
-	else
-	{
-		value = -1;
-		_putchar('-');
-	}
-
-	return (value);
+    	if (n > 0)
+    	{
+        	signvalue = 1;
+        	_putchar('+');
+    	}
+    	else if (n == 0)
+    	{
+        	signvalue = 0;
+        	_putchar('0');
+    	}
+    	else (n < 0)
+    	{
+        	signvalue = -1;
+        	_putchar('-');
+    	}
+    	return signvalue;
 }
