bec7bee33ea23c318f2ee85a43bc2007e1e9e5b5
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-02-17 11:51:35 +0000

    Task completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
-  * main - The entry point of C programs
-  * @void: The function takes no parameters
-  *
-  * Description: The function "printf" writes to stdout.
-  * Return: 0
-  */
+ * main - The entry point of C programs
+ *
+ * Description: Uses the function "printf" to write to stdout.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
