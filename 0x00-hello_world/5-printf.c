82e022161105db6f4d141d094d174e77ecf10ac3
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-02-17 11:56:45 +0000

    Task completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Uses the function "printf" to write to stdout.
+ * Description: Uses the function "printf" to write to stdout
  * Return: Always 0 (Success)
  */
 int main(void)
