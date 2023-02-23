1ecfe6f00c344cff355c6eb87d0901905604a8a6
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-02-23 18:47:46 +0000

    Task completed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int base_ten_numbers = 0;
+	unsigned int base_ten_numbers = 0;
 
 	while (base_ten_numbers < 10)
 	{
-		printf("%i", base_ten_numbers);
+		printf("%u", base_ten_numbers);
 		base_ten_numbers++;
 	}
 	putchar('\n');
