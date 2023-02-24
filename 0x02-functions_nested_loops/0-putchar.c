cb76c87e79eecee3bb8050409d5afbc3497cb8b9
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-02-24 13:29:41 +0000

    Task completed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include "_putchar.c"
 #include "main.h"
 
 /**
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include "_putchar.c"
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
