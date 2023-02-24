45f54bab50b0079ccb0f21198c0ad01899610c36
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-02-24 14:50:46 +0000

    Task completed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * add - The entry point of the function
+ * add - prints the result of the sum of two integers
  * @a: First operand argument
  * @b: Second operand argument
  *
- * Description: This is a function that adds two integers and returns the
- * result.
  * Return: sum value
  */
 int add(int a, int b)
 {
-	int sum = n1 + n2;
+	int sum = a + b;
 
 	return (sum);
 }
