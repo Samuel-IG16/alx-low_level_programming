e9557c10b6c5bb0af84a6db0237ee7c4ae92e989
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-02-24 16:04:20 +0000

    Task completed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int i;
+	int num;
 	long int fibonacci[50], sum = 2;
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 
-	for (i = 2; i < 50; i++)
+	for (num = 2; num < 50; num++)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
-			sum += fibonacci[i];
+		fibonacci[num] = fibonacci[num - 1] + fibonacci[num - 2];
+		if ((fibonacci[num] % 2) == 0 && fibonacci[num] < 4000000)
+			sum += fibonacci[num];
 	}
 	printf("%ld\n", sum);
 
