347ac8fcdeb1b84e6266b5dfa6012bd8b387f38a
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-01 16:08:08 +0000

    Task completed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string,
- * starting with the first character, followed by a new line.
+ * puts2 - prints every other character of a string
  * @str: An input string
- * Return: Nothing
+ *
+ * Return: void
  */
 void puts2(char *str)
 {
@@ -17,6 +17,5 @@ void puts2(char *str)
 
 	for (; i <= len; i += 2)
 		_putchar(str[i]);
-
 	_putchar('\n');
 }
