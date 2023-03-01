f7c6254065b1739726f4da961b6dadb4eb00d50a
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-01 16:36:31 +0000

    Task completed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * _atoi - a function that converts string to integer
+ * _atoi - converts string to integer
  * @s: An input string
+ *
  * Return: integer from conversion
  */
 int _atoi(char *s)
