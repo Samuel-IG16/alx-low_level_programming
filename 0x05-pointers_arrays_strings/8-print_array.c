faa15b8d972d4b64f2ea2d31703be8dab6177875
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-01 16:32:28 +0000

    Task completed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array - a function that prints n elements of an array of integers,
- * followed by a new line.
+ * print_array - prints n elements of an array of integers
  * @a: an input array
  * @n: an input integer
- * Return: Nothing
+ *
+ * Return: void
  */
 void print_array(int *a, int n)
 {
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcpy - makes a copy of a string
