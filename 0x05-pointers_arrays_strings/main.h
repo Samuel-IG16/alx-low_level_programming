4878a1b9371c0d7a1de462720425ed4cd419ad6e
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-01 15:50:11 +0000

    Task completed

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "_putchar.c"
+#include <stdio.h>
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int _putchar();
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -10,3 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+#endif
