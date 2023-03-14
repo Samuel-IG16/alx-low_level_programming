57fccf9638a123c0c3dffdfa68c9e5920ddf0d64
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-14 23:17:09 +0000

    Task completed

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 /**
  * set_string - sets value of a pointer to a char
  * @s: double pointer to a string
- * @to: pointer to the string to set s to
+ * @to: the pointer to the string to set s to
  *
  * Return: void
  */
