1a4f1366b0dcd8454cae5f984709ed4197f51903
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-14 00:19:14 +0000

    Task completed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,8 @@
-#include "main.h"
-
 /**
  * _strcmp - a function that compares two strings.
  * @s1: an input string
  * @s2: an input string
+ *
  * Return: The difference s1 - s2
  */
 int _strcmp(char *s1, char *s2)
