868ed7a7b6b07a6a365862b9e9f9778c0f629d7a
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-14 00:28:14 +0000

    Task completed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,9 @@
-#include "main.h"
-
 /**
- * string_toupper - a function that changes all lowercase letters of a  string
+ * string_toupper - changes all lowercase letters of a string
  * to upper case
  * @s: an input string
- * Return: char pointer to converted string
+ *
+ * Return: pointer to converted string
  */
 char *string_toupper(char *s)
 {
