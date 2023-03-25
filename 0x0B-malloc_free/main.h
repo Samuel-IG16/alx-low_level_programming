f8874ab356ee22977b55d1e9468ea9745211c7ab
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-03-25 03:06:52 +0000

    Task completed

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "_putchar.c"
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -6,3 +11,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+#endif
